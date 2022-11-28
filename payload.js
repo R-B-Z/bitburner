5335c2948728fac9b21bb1940b1d6b64ab4759b7
R-B-Z <johndoe@example.com>
2022-11-27 23:20:41 -0500

    Fixed parenthesis / Added custom logs

--- payload.js
+++ payload.js
@@ -2,20 +2,24 @@ import { populate } from "server.js";
 /** @param {NS} ns */
 
 export async function main(ns) {
+	ns.disableLog('ALL');
+	ns.clearLog();
 
 	let targets = populate(ns, ['joesguns', 'n00dles']);
-	
+
 	while (true) {
-			await targets[0].lend();
+		await targets[0].lend();
 
-			let target = targets[0];
+		let target = targets[0];
 
-			if (!targets[0].get_root()) target = targets[1];
+		if (!targets[0].get_root()) target = targets[1];
 
-			if (target.get_security_level() > target.get_security_threshold) await target.exec_weaken();
-			else if (target.get_money_available() < target.get_money_threshold) await target.exec_grow();
-			else await target.exec_hack();	
-			await ns.sleep(1);		
+		ns.print(`Server: ${target.hostname}\nSecurity: ${target.get_security_level()}\nMoney: ${target.get_money_available()}\nThreshold: ${target.get_money_threshold()}`);
+
+		if (target.get_security_level() > target.get_security_threshold()) await target.exec_weaken();
+		else if (target.get_money_available() < target.get_money_threshold()) await target.exec_grow();
+		else await target.exec_hack();
+		await ns.sleep(1);
 	}
 
 }
