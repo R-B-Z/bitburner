7ebf5cb345166b1fc320e348a9c827396c823bed
R-B-Z <johndoe@example.com>
2022-11-28 07:28:47 -0500

    Added linebreak to log

--- payload.js
+++ payload.js
@@ -14,7 +14,7 @@ export async function main(ns) {
 
 		if (!targets[0].get_root()) target = targets[1];
 
-		ns.print(`Server: ${target.hostname}\nSecurity: ${target.get_security_level()}\nMoney: ${target.get_money_available()}\nThreshold: ${target.get_money_threshold()}`);
+		ns.print(`\nServer: ${target.hostname}\nSecurity: ${target.get_security_level()}\nMoney: ${target.get_money_available()}\nThreshold: ${target.get_money_threshold()}`);
 
 		if (target.get_security_level() > target.get_security_threshold()) await target.exec_weaken();
 		else if (target.get_money_available() < target.get_money_threshold()) await target.exec_grow();
