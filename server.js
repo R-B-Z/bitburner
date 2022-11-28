88162437e36f2a4abb61d41277dbcca083682146
R-B-Z <johndoe@example.com>
2022-11-28 16:41:10 -0500

    Bugs fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ export function server(ns, host, ram) {
 		run_scripts: function () { this.fetch_dependencies(); for (let script of this.scripts) { if (!ns.isRunning(script, this.hostname)) { ns.scp(script, this.hostname); if (script == 'payload.js') { ns.exec(script, this.hostname, this.get_thread_count()); } else if (this.hostname == 'home') { ns.exec(script, this.hostname, 1); } } } },
 		stop_scripts: function () { if (this.get_purchased_servers().includes(this.hostname)) ns.killall(this.hostname); },
 		upgrade: function () { this.memory = this.get_memory_upgrade(); this.stop_scripts(); this.destroy(); this.purchase(); this.run_scripts(); },
-		infect: function () { if (this.hostname.includes(this.owned_prefix)) this.infected = true; if (!this.infected && this.get_opened_ports() <= this.get_required_ports()) { if (this.exploits.length >= this.get_required_ports()) { if (ns.getHackingLevel() >= this.get_required_level()) { if (this.exploits.length >= 1) ns.brutessh(this.hostname); if (this.exploits.length >= 2) ns.ftpcrack(this.hostname); if (this.exploits.length >= 3) ns.relaysmtp(this.hostname); if (this.exploits.length >= 4) ns.httpworm(this.hostname); if (this.exploits.length >= 5) ns.sqlinject(this.hostname); } } } if (this.get_opened_ports() >= this.get_required_ports() && !this.get_root()) { ns.nuke(this.hostname); this.infected = true; } if (this.get_root()) this.infected = true; if (this.infected) { if (this.get_thread_count() > 0) this.run_scripts(); return 1; } else { return 0; } },
+		infect: function () { if (this.hostname.includes(this.owned_prefix)) this.infected = true; if (!this.infected && this.get_opened_ports() <= this.get_required_ports()) { if (ns.getHackingLevel() >= this.get_required_level()) { if (ns.fileExists(this.exploits[0], 'home')) ns.brutessh(this.hostname); if (ns.fileExists(this.exploits[1], 'home')) ns.ftpcrack(this.hostname); if (ns.fileExists(this.exploits[2], 'home')) ns.relaysmtp(this.hostname); if (ns.fileExists(this.exploits[3], 'home')) ns.httpworm(this.hostname); if (ns.fileExists(this.exploits[4], 'home')) ns.sqlinject(this.hostname); } } if (this.get_opened_ports() >= this.get_required_ports() && !this.get_root()) { ns.nuke(this.hostname); this.infected = true; } if (this.get_root()) this.infected = true; if (this.infected) { if (this.get_thread_count() > 0) this.run_scripts(); return 1; } else { return 0; } },
 		lend: async function () { await ns['share'](); },
 		exec_hack: async function () { await ns['hack'](this.hostname); },
 		exec_grow: async function () { await ns['grow'](this.hostname); },
@@ -43,10 +43,12 @@ export function server(ns, host, ram) {
 
 export function populate(ns, hosts, ram = 8) {
 	let servers = [];
+	ns.print(hosts);
 	for (let host of hosts) {
 		if (ns.serverExists(host)) servers.push(server(ns, host, ns.getServerMaxRam(host)));
 		else servers.push(server(ns, host, ram));
 	}
+	
 	return servers;
 }
 
@@ -64,7 +66,7 @@ export async function upgrade_all(ns, hosts) {
 }
 
 export async function infect_all(ns, hosts) {
-	for (let victim of hosts) { if (!victim.infected) victim.infect(); } await ns.sleep(1);
+	for (let victim of hosts) { if (!victim.infected && ns.serverExists(victim.hostname)) victim.infect(); } await ns.sleep(1);
 }
 
 export async function purchase_all(ns) {
