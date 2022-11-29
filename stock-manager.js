e951bccfb0414c5acee93d919d61358fee064de6
R-B-Z <johndoe@example.com>
2022-11-29 06:40:47 -0500

    Bug fixes

--- stock-manager.js
+++ stock-manager.js
@@ -7,12 +7,12 @@ export async function main(ns) {
 	ns.disableLog('ALL');
 	ns.clearLog();
 
-	if (ns.stock.hasWSEAccount()) {
-		let stonks = populate(ns, stocks(ns));
-
-		while (true) {
+	while (true) {
+		if (ns.stock.hasWSEAccount()) {
+			let stonks = populate(ns, stocks(ns));
 			manage_portfolio(ns, stonks);
-			await ns.sleep(1);
 		}
+		await ns.sleep(1);
 	}
+	
 }
