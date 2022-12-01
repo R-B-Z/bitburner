d941f9848f27d68333f66e45208a35fcbd1c96ef
R-B-Z <johndoe@example.com>
2022-12-01 07:23:50 -0500

    Bug fixes

--- stock-manager.js
+++ stock-manager.js
@@ -1,15 +1,16 @@
 import { stocks } from "game_info.js";
-import { populate, manage_portfolio } from "stock.js";
+import { populate, manage_portfolio, exchange } from "stock.js";
 /** @param {NS} ns */
 
 export async function main(ns) {
 
 	ns.disableLog('ALL');
 	ns.clearLog();
+	let stonks = [];
 
 	while (true) {
-		if (ns.stock.hasWSEAccount()) {
-			let stonks = populate(ns, stocks(ns));
+		if (exchange.get_WSE(ns)) {
+			stonks = populate(ns, stocks(ns));
 			manage_portfolio(ns, stonks);
 		}
 		await ns.sleep(1);
